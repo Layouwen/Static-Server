9462b314a04e4f176bd406d7905b700981715b15
layouwen <layouwen@gmail.com>
2020-06-11 22:10:09 +0800

    auto spot path

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var url = require('url')
 var port = process.argv[2]
 
 if (!port) {
-    console.log('请指定端口号好不啦？\nnode server.js 8888 这样不会吗？')
+    console.log('请输入指定端口。如：\nnode server.js 8888')
     process.exit(1)
 }
 
@@ -19,18 +19,23 @@ var server = http.createServer(function (request, response) {
     var query = parsedUrl.query
     var method = request.method
 
-    /******** 从这里开始看，上面不要看 ************/
-
-    console.log('有个傻子发请求过来啦！路径（带查询参数）为：' + pathWithQuery)
+    /******** main start ************/
 
     response.statusCode = 200
     response.setHeader('Content-Type', 'text/html;charset=utf-8')
-    const x = path
-    response.write(fs.readFileSync(`./public${x}`))
+    const filePath = path === '/' ? '/index.html' : path
+    let content
+    try {
+        content = fs.readFileSync(`./public${filePath}`)
+    } catch (error) {
+        content = '文件路径不存在'
+        response.statusCode = 404
+    }
+    response.write(content)
     response.end()
 
-    /******** 代码结束，下面不要看 ************/
+    /******** main end ************/
 })
 
 server.listen(port)
-console.log('监听 ' + port + ' 成功\n请用在空中转体720度然后用电饭煲打开 http://localhost:' + port)
+console.log('监听 ' + port + ' 成功！请输入下列地址访问\nhttp://localhost:' + port)
